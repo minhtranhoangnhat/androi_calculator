3c98c8fedf3d8152fd7493fd988c3d16b2191fdb
mitrahonha <minhtrhoangnhat@gmail.com>
2025-03-14 23:15:48 +0700

    test

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,143 @@
+package com.example.calculator
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+    private var canAddOperation = false
+    private var canAddDecimal = true
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun numberAction(view: View) {
+        if (view is Button) {
+            if (view.text == ".") {
+                if (canAddDecimal)
+                    binding.workingtv.append(view.text)
+                canAddDecimal = false
+            } else
+                binding.workingtv.append(view.text)
+            canAddOperation = true
+        }
+    }
+
+    fun operatorAction(view: View) {
+        if (view is Button && canAddOperation) {
+            binding.workingtv.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+
+    fun allClearAction(view: View) {
+        binding.workingtv.text = ""
+        binding.resulttv.text = ""
+    }
+
+    fun backSpaceAction(view: View) {
+        val length = binding.workingtv.text.length
+        if (length > 0)
+            binding.workingtv.text = binding.workingtv.text.subSequence(0, length - 1)
+    }
+
+    fun clearEntryAction(view: View) {
+        binding.workingtv.text = ""
+    }
+
+    fun equalsAction(view: View){
+        binding.resulttv.text = calculateResults()
+    }
+
+    private fun calculateResults(): String{
+        val digitsOperators = digitsOperators()
+        if(digitsOperators.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if(timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+
+        return result.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float{
+        var result = passedList[0] as Float
+
+        for(i in passedList.indices){
+            if(passedList[i] is Char && i != passedList.lastIndex){
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if(operator == '+') result += nextDigit
+                if(operator == '-') result -= nextDigit
+            }
+        }
+
+        return result
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+        for(i in passedList.indices){
+            if(passedList[i] is Char && i != passedList.lastIndex && i < restartIndex){
+                var operator = passedList[i]
+                var prevDigit = passedList[i - 1] as Float
+                var nextDigit = passedList[i + 1] as Float
+                when(operator){
+                    'x' ->{
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' ->{
+                        newList.add(prevDigit / nextDigit)
+                    }
+                    else ->{
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+            if(i > restartIndex)
+                newList.add(passedList[i])
+        }
+
+        return newList
+    }
+
+    private fun digitsOperators(): MutableList<Any>{
+        var list = mutableListOf<Any>()
+        var currentDigit = ""
+        for(character in binding.workingtv.text){
+            if(character.isDigit() || character == '.')
+                currentDigit += character
+            else {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+
+        if(currentDigit != "") list.add(currentDigit.toFloat())
+
+        return list
+    }
+}
